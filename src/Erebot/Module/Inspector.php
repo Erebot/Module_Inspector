d845aa18e65f3e71b48a5b461ed9620b18e22844
François Poirotte <clicky@erebot.net>
2014-02-28 00:12:20 +0100

    Initial commit

---
+++ src/Erebot/Module/Inspector.php
@@ -0,0 +1,50 @@
+<?php
+/*
+    This file is part of Erebot.
+
+    Erebot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Erebot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * \brief
+ *      A module that provides debugging helpers for Erebot
+ *      using introspection and XML-RPC over SSH.
+ */
+class   Erebot_Module_Inspector
+extends Erebot_Module_Base
+{
+    /// A list of handlers registered by this module.
+    protected $_handlers;
+
+    /// A list of triggers registered by this module.
+    protected $_triggers;
+
+
+    /**
+     * This method is called whenever the module is (re)loaded.
+     *
+     * \param int $flags
+     *      A bitwise OR of the Erebot_Module_Base::RELOAD_*
+     *      constants. Your method should take proper actions
+     *      depending on the value of those flags.
+     *
+     * \note
+     *      See the documentation on individual RELOAD_*
+     *      constants for a list of possible values.
+     */
+    public function _reload($flags)
+    {
+    }
+}
+
---
+++ src/Erebot/Module/Inspector/Bridge.php
@@ -0,0 +1,169 @@
+<?php
+
+/*
+* This file is part of pssht.
+*
+* (c) François Poirotte <clicky@erebot.net>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Clicky\Pssht;
+
+use Clicky\Pssht\Connection;
+use Clicky\Pssht\Transport;
+use Clicky\Pssht\MessageInterface;
+use Clicky\Pssht\Messages\CHANNEL\DATA;
+
+/**
+ * Bridge between pssht \& XRL.
+ */
+class XRL implements \Clicky\Pssht\HandlerInterface
+{
+    /// SSH connection layer.
+    protected $connection;
+
+    /// Internal buffer.
+    protected $buffer;
+
+    /// XML-RPC server.
+    protected $server;
+
+    /**
+     * Construct a new bridge between pssht \& XRL.
+     *
+     *  \param Transport $transport
+     *      SSH transport layer.
+     *
+     *  \param Connection $connection
+     *      SSH connection layer.
+     *
+     *  \param MessageInterface $message
+     *      SSH message being handled.
+     */
+    public function __construct(
+        Transport $transport,
+        Connection $connection,
+        MessageInterface $message
+    ) {
+        $this->connection   = $connection;
+        $this->buffer       = new \Clicky\Pssht\Buffer();
+
+        require_once(
+            dirname(dirname(__DIR__)) .
+            DIRECTORY_SEPARATOR . 'XRL' .
+            DIRECTORY_SEPARATOR . 'src' .
+            DIRECTORY_SEPARATOR . 'XRL' .
+            DIRECTORY_SEPARATOR . 'Autoload.php'
+        );
+        \spl_autoload_register(array('XRL_Autoload', 'load'));
+        $this->server       = new \XRL_Server();
+
+        $connection->setHandler(
+            $message,
+            \Clicky\Pssht\Messages\CHANNEL\DATA::getMessageId(),
+            $this
+        );
+
+        $this->server->foo = function () {
+            return 42;
+        };
+        $this->prompt($transport, $message);
+
+        if ($message instanceof \Clicky\Pssht\Messages\CHANNEL\REQUEST\Exec) {
+            $message = new \Clicky\Pssht\Messages\CHANNEL\DATA(
+                $message->getChannel(),
+                $message->getCommand()
+            );
+            $context = array();
+            $encoder = new \Clicky\Pssht\Wire\Encoder();
+            $message->serialize($encoder);
+            $this->handle(
+                \Clicky\Pssht\Messages\CHANNEL\DATA::getMessageId(),
+                new \Clicky\Pssht\Wire\Decoder($encoder->getBuffer()),
+                $transport,
+                $context
+            );
+        }
+    }
+
+    /**
+     * Prompt user for next command.
+     *
+     *  \param Transport $transport
+     *      Transport layer for the SSH connection.
+     *
+     *  \param MessageInterface $message
+     *      Message being handled.
+     */
+    protected function prompt(
+        \Clicky\Pssht\Transport $transport,
+        \Clicky\Pssht\MessageInterface $message
+    ) {
+        // Display prompt on stderr.
+        $response = new \Clicky\Pssht\Messages\CHANNEL\EXTENDED\DATA(
+            $this->connection->getChannel($message),
+            \Clicky\Pssht\Messages\CHANNEL\EXTENDED\DATA::SSH_EXTENDED_DATA_STDERR,
+            '>>> '
+        );
+        $transport->writeMessage($response);
+    }
+
+    public function handle(
+        $msgType,
+        \Clicky\Pssht\Wire\Decoder $decoder,
+        \Clicky\Pssht\Transport $transport,
+        array &$context
+    ) {
+        $message = \Clicky\Pssht\Messages\CHANNEL\DATA::unserialize($decoder);
+        $this->buffer->push(trim($message->getData()));
+        while (true) {
+            $call = $this->buffer->get('</methodCall>');
+            if ($call === null) {
+                break;
+            }
+
+            // Display the command's result.
+            $xmlResponse    = $this->server->handle($call);
+#            $response       = new \Clicky\Pssht\Messages\CHANNEL\DATA(
+#                $this->connection->getChannel($message),
+#                ((string) $xmlResponse) . PHP_EOL
+#            );
+#            $response = new \Clicky\Pssht\Messages\CHANNEL\EXTENDED\DATA(
+#                $this->connection->getChannel($message),
+#                1,
+#                ((string) $xmlResponse) . PHP_EOL
+#            );
+#            $transport->writeMessage($response);
+
+            $transport->writeMessage(
+                new \Clicky\Pssht\Messages\IGNORE(
+                    'pssht:' . ((string) $xmlResponse)
+                )
+            );
+        }
+
+        if (count($this->buffer) > 4096) {
+            $call = $this->buffer->get(0);
+
+            // Display the command's result.
+            $xmlResponse    = $this->server->handle($call);
+#            $response       = new \Clicky\Pssht\Messages\CHANNEL\DATA(
+#                $this->connection->getChannel($message),
+#                ((string) $xmlResponse) . PHP_EOL
+#            );
+#            $transport->writeMessage($response);
+            $transport->writeMessage(
+                new \Clicky\Pssht\Messages\IGNORE(
+                    'pssht:' . ((string) $xmlResponse)
+                )
+            );
+        }
+
+        // Display the prompt again.
+        if (count($this->buffer) === 0) {
+            $this->prompt($transport, $message);
+        }
+    }
+}
